0f0e0add6c6b2c0b2c8d39b1aa7bb608367cdc37
codegino <carloginocatapang@gmail.com>
2022-03-26 08:59:17 +0800

    refactor: Moved hardhat related code to another folder

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -7,7 +7,7 @@ const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    rinkedby: {
+    rinkeby: {
       url: ALCHEMY_API_KEY_URL,
       accounts: [RINKEBY_PRIVATE_KEY],
     },
